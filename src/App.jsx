c197fd1b3a55d6ce7769a536ed1a95f7604c2a42
Jobayer-ak <jobayer.ak@gmail.com>
2023-11-19 10:29:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import FlightBook from './components/FlightBook';
+
+function App() {
+  return (
+    <>
+      <FlightBook />
+    </>
+  );
+}
+
+export default App;
